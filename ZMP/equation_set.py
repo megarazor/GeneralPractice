72355d36a48df6922b1cd11b9b622fca49a0d856
PHAM Van Nam <vannampham1301@gmail.com>
2021-04-22 13:07:48 +0900

    a

--- ZMP/equation_set.py
+++ ZMP/equation_set.py
@@ -1,11 +1,9 @@
 
 eqs= []
 new_eq= input()
-# eq.append(input())
 while(new_eq != ""):
     eqs.append(new_eq)
     new_eq= input()
-print(eqs)
 
 eqs_cnt= len(eqs)
 var_cnt= eqs_cnt
@@ -17,10 +15,8 @@ for eq in eqs:
         if c >= 'a' and c <= 'z':
             if c not in vars:
                 vars.append(c)
-# vars= sorted(vars)
-# print(vars)
+vars= sorted(vars)
 matrix = [[0]* (var_cnt + 1) for _ in range(var_cnt)]
-# print("matrix before:", matrix)
 
 for i in range(len(eqs)):
     left= 1
@@ -32,22 +28,26 @@ for i in range(len(eqs)):
             sign= 1
             num= 1        
             if j > 1:
-                if eq[j - 1] >= '-':
+                # print("getting num/sign for var {0} mid sentence".format(var))
+                # print("before {0}: {1}".format(var, eq[j-1]))
+                if eq[j - 1] == '-':
                     sign= -1
                 elif eq[j - 1] >= '0' and eq[j - 1] <= '9':
-                    num= int(eq[i - 1])
+                    # print("got num {0} for var {1}".format(eq[j-1], var))
+                    num= int(eq[j - 1])
                     if eq[j - 2] >= '-':
                         sign= -1
             elif j > 0:
                 if eq[j - 1] >= '0' and eq[j - 1] <= '9':
                     num= int(eq[j - 1])
+                    # print("got num {0} for var {1}".format(num, var))
                 elif eq[j - 1] >= '-':
                     sign= -1
             index= vars.index(var)
             # print("sign, num, left:", sign, num, left)
 
             matrix[i][index]= sign * num * left
-            # print("matrix[{0}][{1}]= {2}*{3}*{4}".format(i, index, sign, num, left))
+            # print("matrix[{0}][{1}]= {2}*{3}*{4}   {5}".format(i, index, sign, num, left, var))
             # print(var, "has factor:", sign * num * left)
         elif eq[j] == '=':
             left= -1
@@ -64,6 +64,35 @@ for i in range(len(eqs)):
                     sign= -1
                 matrix[i][var_cnt]= int(eq[j]) * sign * left * -1
                 # print("matrix[{0}][{1}]= {2}".format(i, var_cnt, eq[j]))
-    print()     
+    # print()     
             
 print(matrix)
+
+for col in range(len(matrix[0]) - 1):
+    for row in range(col, len(matrix)):
+        if row == col:
+            n= matrix[row][col]
+            if n == 0:
+                for i in range(row + 1, len(matrix)):
+                    if matrix[i][col] != 0:
+                        tmp= matrix[i]
+                        matrix[i]= matrix[row]
+                        matrix[row]= tmp
+                        n= matrix[row][col]
+            for i in range(len(matrix[row])):
+                matrix[row][i]/= n
+        else:
+            n= matrix[row][col]
+            for i in range(len(matrix[row])):
+                matrix[row][i]-= n * matrix[col][i]
+
+print(matrix)
+        
+res= [0] * var_cnt
+for i in range(len(res) - 1, -1, -1):
+    n= 0
+    for j in range(i + 1, len(matrix[i]) - 1):
+        n+= matrix[i][j] * res[j]
+    res[i]= matrix[i][len(matrix[i]) - 1] - n
+
+print(res)
